d3cb2d1db8864b2143a2431fd11ccf3e5b013132
pevec1 <pevec1@yandex.ru>
2023-07-06 07:10:04 +0300

    1.3.4

--- js/ticket.js
+++ js/ticket.js
@@ -1,16 +1,7 @@
-class Destroyer{
-    constructor(){
-    }
-destroyer(){
+  value.getTicket()
         let sales = JSON.parse(localStorage['sales'])
         localStorage.clear()
         //delete localStorage['list']
         console.log('list cleared')
         localStorage['sales'] = JSON.stringify(sales)
-    }
-}
 
-  value.getTicket()
-
-let obj = new Destroyer()
-obj.destroyer()
